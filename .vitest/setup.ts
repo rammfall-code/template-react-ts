122c2785ca402333e0d42322f0b899e0cb2d6f46
rammfall <15bogdan17@gmail.com>
2026-03-11 17:52:45 +0200

    Update deps

--- .vitest/setup.ts
+++ .vitest/setup.ts
@@ -1,7 +1,22 @@
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
 afterEach(() => {
   cleanup();
+});
+
+// eslint-disable-next-line no-undef
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
 });
